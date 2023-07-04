f8cacf3d66676841cee74a28513bc09e24cedfef
thorne7 <darienhallam@gmail.com>
2023-07-04 19:41:33 +0930

    inquirer prompt questions

--- server.js
+++ server.js
@@ -1,2 +1,63 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+const {
+  viewAllDepartments,
+  viewAllRoles,
+  viewAllEmployees,
+  addDepartment,
+  addRole,
+  addEmployee,
+  updateEmployeeRole
+} = require('./queries');
+
+// Function to prompt the user for their desired action
+function promptUser() {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    })
+    .then(answer => {
+      switch (answer.action) {
+        case 'View all departments':
+          viewAllDepartments();
+          break;
+        case 'View all roles':
+          viewAllRoles();
+          break;
+        case 'View all employees':
+          viewAllEmployees();
+          break;
+        case 'Add a department':
+          addDepartment();
+          break;
+        case 'Add a role':
+          addRole();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Update an employee role':
+          updateEmployeeRole();
+          break;
+        case 'Exit':
+          console.log('Goodbye!');
+          process.exit();
+      }
+    });
+}
+
+// Start the application by prompting the user
+promptUser();
